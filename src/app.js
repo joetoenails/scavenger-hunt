ff3149e2de164adc7e457346ed799e60d34f8bdc
joetoenails <joe.d.tonelli@gmail.com>
2021-01-23 02:23:49 -0500

    countdown working, match predict to search working,

--- src/app.js
+++ src/app.js
@@ -1,30 +1,51 @@
 import React from "react";
-import GamePage from "./gamePage";
+import * as tfjs from "@tensorflow/tfjs";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import { WebcamCanvas } from "./webcamCanvas";
+
+const constraints = { audio: false, video: { width: 400, height: 400 } };
 
 class WelcomeLoader extends React.Component {
   constructor() {
     super();
-    this.state = { isModelLoaded: false };
+    this.state = { isModelLoaded: false, model: null, videoSource: null };
+    this.image = React.createRef();
+    this.getMedia = this.getMedia.bind(this);
+    this.videoRef = React.createRef();
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isModelLoaded: true });
-    }, 3000);
+  async componentDidMount() {
+    const model = await mobilenet.load();
+    this.getMedia(constraints);
+    this.setState({ isModelLoaded: true, model: model });
+  }
+
+  async getMedia(constraints) {
+    let stream = null;
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+      this.setState({ videoSource: stream });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
     if (this.state.isModelLoaded) {
       return (
         <div>
-          <GamePage />
+          <WebcamCanvas
+            model={this.state.model}
+            videoSource={this.state.videoSource}
+          />
         </div>
       );
     }
 
     return (
       <div>
-        <h1>Model is loading...</h1>
+        <h1>Game is loading...don't forget to grant us webcam access!</h1>
       </div>
     );
   }
--- src/webcamCanvas.js
+++ src/webcamCanvas.js
@@ -1,45 +1,120 @@
 import React, { useEffect, useRef } from "react";
 import { render } from "react-dom";
+import GamePage from "./gamePage";
+import labels from "../dist/imagenetLabels";
+
+function randomNum() {
+  return Math.floor(Math.random() * 999);
+}
+
+function getLabels() {
+  return [
+    labels[randomNum()].split(",")[0],
+    labels[randomNum()].split(",")[0],
+    labels[randomNum()].split(",")[0],
+  ];
+}
 
 class WebcamCanvas extends React.Component {
-  constructor() {
-    super();
-    this.state = { videoSource: null };
-    this.getMedia = this.getMedia.bind(this);
-    this.stopVid = this.stopVid.bind(this);
-    this.constraints = { audio: false, video: { width: 500, height: 500 } };
-    this.videoRef = React.createRef();
+  constructor(props) {
+    super(props);
+    //state
+    this.state = {
+      videoLoaded: false,
+      playerReady: false,
+      searchItems: getLabels(),
+    };
+    //refs
+    this.video = React.createRef();
+    this.counter = React.createRef();
+    this.predictions = React.createRef();
+
+    //bindings
+    this.countdown = this.countdown.bind(this);
+    this.readyUp = this.readyUp.bind(this);
+    this.checkMatch = this.checkMatch.bind(this);
+    this.predict = this.predict.bind(this);
+    this.setVidSrc = this.setVidSrc.bind(this);
   }
 
-  //VIDEO NODE = THIS.VIDEOREF.CURRENT
-  componentDidMount() {
-    this.getMedia(this.constraints);
-  }
-  async getMedia(constraints) {
-    let stream = null;
-
-    try {
-      stream = await navigator.mediaDevices.getUserMedia(constraints);
-      this.setState({ videoSource: stream });
-      this.videoRef.current.srcObject = stream;
-    } catch (err) {
-      console.log(err);
-    }
+  setVidSrc() {
+    this.video.current.srcObject = this.props.videoSource;
   }
 
-  stopVid() {}
+  async predict() {
+    const predictions = await this.props.model.classify(this.video.current);
+    this.predictions.current = predictions;
+    console.log(this.predictions.current);
+  }
+
+  readyUp() {
+    this.setState({ playerReady: true });
+    this.countdown();
+  }
+  countdown() {
+    const timer = setInterval(() => {
+      this.counter.current.innerHTML =
+        Number(this.counter.current.innerHTML) - 1;
+
+      if (this.counter.current.innerHTML === "0") {
+        clearInterval(timer);
+        this.counter.current.innerHTML = "OUT OF TIME!";
+      }
+    }, 1000);
+  }
+
+  checkMatch(predictions, searchItems) {
+    let found = false;
+    const top1 = predictions[0];
+    const top2 = predictions[1];
+
+    searchItems.forEach((item) => {
+      console.log(item);
+      if (item === top1.className || item === top2.className) {
+        found = true;
+      }
+    });
+    return found;
+  }
 
   render() {
-    console.log(this.state.videoSource);
+    if (this.video.current) {
+      this.setVidSrc();
+    }
+
     return (
       <div>
-        {this.state.videoSource && (
-          <div>
-            <p>trying.</p>
-            <video ref={this.videoRef} autoPlay={true}></video>
-            <button onClick={this.stopVid}>Stop Vid</button>
-          </div>
-        )}
+        <div>
+          <video ref={this.video} autoPlay={true}></video>
+        </div>
+        <button onClick={this.predict}> Predict</button>
+        <button
+          onClick={() => {
+            this.checkMatch(this.predictions.current, this.state.searchItems);
+          }}
+        >
+          Check Match
+        </button>
+        <div>
+          {this.state.playerReady ? (
+            <div>
+              <p>You have 15 seconds to find one of these items!</p>
+              <h1 ref={this.counter}>15</h1>
+              {this.state.searchItems.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <ol>{item}</ol>
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div>
+              <h1>The timer begins when you click ready!</h1>
+              <button onClick={this.readyUp}> READY! </button>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
